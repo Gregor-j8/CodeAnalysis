5d8ced73536aea5a46bb7536a9dd8495ce56cb60
gregor-J8 <gregor.johnson@outlook.com>
2025-08-18 15:47:24 -0500

    added workers and updated logic for tracking file type

--- Worker/Scanner.go
+++ Worker/Scanner.go
@@ -4,12 +4,23 @@ import (
 	"fmt"
 	"os"
 	"path/filepath"
+	"runtime"
 	"strings"
+	"sync"
 
 	"github.com/spf13/cobra"
 )
 
 func Scan(path string) {
+	allowedExts := map[string]bool{
+		".go":  true,
+		".cs":  true,
+		".py":  true,
+		".js":  true,
+		".ts":  true,
+		".jsx": true,
+		".tsx": true,
+	}
 	fileInfo, err := os.Stat(path)
 	if os.IsNotExist(err) {
 		cobra.CheckErr(fmt.Errorf("path does not exist: %s", path))
@@ -18,39 +29,40 @@ func Scan(path string) {
 		cobra.CheckErr(err)
 	}
 
+	if !fileInfo.IsDir() {
+		ext := filepath.Ext(path)
+		if allowedExts[ext] {
+			fmt.Println("Processing file:", path)
+		}
+		return
+	}
+
 	if fileInfo.IsDir() {
-		fmt.Println("Scanning directory:", path)
+		jobs := make(chan string, 10)
+		var wg sync.WaitGroup
+		numWorkers := runtime.NumCPU() * 3
+
+		for i := 0; i < numWorkers; i++ {
+			wg.Add(1)
+			go workerPool(&wg, jobs)
+		}
 
 		err := filepath.WalkDir(path, func(filePath string, entry os.DirEntry, err error) error {
 			if err != nil {
 				return err
 			}
-			if entry.IsDir() && (entry.Name() == "node_modules" || entry.IsDir() && strings.HasPrefix(entry.Name(), ".")) {
+			if entry.IsDir() && (entry.Name() == "node_modules" || strings.HasPrefix(entry.Name(), ".")) {
 				return filepath.SkipDir
 			}
-			analyzeFile(filePath)
+			ext := filepath.Ext(filePath)
+			if allowedExts[ext] {
+				jobs <- filePath
+			}
 			return nil
 		})
 		cobra.CheckErr(err)
-	} else {
-		analyzeFile(path)
-	}
-}
 
-func analyzeFile(path string) {
-	ext := filepath.Ext(path)
-	switch ext {
-	case ".go":
-		fmt.Println("run Go analysis on file:", path)
-	case ".cs":
-		fmt.Println("run Go analysis on file:", path)
-	case ".py":
-		fmt.Println("run Python analysis on file:", path)
-	case ".js", ".ts":
-		fmt.Println("run JavaScript/TypeScript analysis on file:", path)
-	case ".jsx", ".tsx":
-		fmt.Println("run React JavaScript/TypeScript analysis on file:", path)
-	default:
-		return
+		close(jobs)
+		wg.Wait()
 	}
 }
--- Worker/WorkerPool.go
+++ Worker/WorkerPool.go
@@ -1,1 +1,14 @@
 package scanner
+
+import (
+	"fmt"
+	"sync"
+)
+
+func workerPool(wg *sync.WaitGroup, jobs <-chan string) {
+	defer wg.Done()
+
+	for job := range jobs {
+		fmt.Println("Processing job:", job)
+	}
+}
